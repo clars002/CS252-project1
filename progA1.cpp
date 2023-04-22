4619a54661df517c5ce8b3ca20f3d87ab154140c
Caelan Larsen <clars002@odu.edu>
2023-04-22 18:42:26 -0400

    added source code

---
+++ progA1.cpp
@@ -0,0 +1,9 @@
+#include "progA1.h"
+#include "utilities.h"
+// OSR is cool. Also, if you are reading this, you need to respect the privacy of others more.
+using namespace std;
+
+void proclaim (const string& st)
+{
+  announce (st + "!");
+}
